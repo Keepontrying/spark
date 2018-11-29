2e63b7f94422722a91b4b03f8eaf0968f63ea56f
liangyuwu <yuwuliang@yeah.net>
2018-11-29 20:43:22 +0800

    Initial commit

---
+++ src/main/java/com/lyw/spark/controller/SparkController.java
@@ -0,0 +1,14 @@
+package com.lyw.spark.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 类描述：spark
+ *
+ * @author liangyuwu
+ * @Time 2018/11/29 16:18
+ */
+@RestController
+public class SparkController {
+
+}
---
+++ src/main/scala/com/lyw/spark/SparkApp.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.Source
+import scala.tools.nsc.io.JFile
+
+class SparkApp{
+
+  def main(args: Array[String]) {
+    myFunc("myApp")
+
+  }
+
+
+  def myFunc(appName: String){
+    val sparkConf = new SparkConf().setAppName(appName).setMaster("spark://172.24.5.64:8180")
+    val sparkContext = new SparkContext(sparkConf)
+    val textFile = sparkContext.textFile("D://export//abc.html")
+    val aCount = textFile.filter(line => line.contains("a")).count()
+    print(aCount)
+  }
+
+  def analyText(filePath:String): String ={
+
+    val stringBuilder = new StringBuilder()
+    val file = Source.fromFile(new JFile(filePath))
+    for(line <- file.getLines()){
+      stringBuilder.append(line+"\n")
+    }
+    return stringBuilder.toString()
+  }
+}
